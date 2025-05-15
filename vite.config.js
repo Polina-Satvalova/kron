03af67e100039b538b784de716fa61df23d5c316
Polina <Polinasatvalova@gmail.com>
2025-05-15 17:56:22 +0300

    change config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/kron/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
